718de64317317d2a3df4f91e2575cb4da46f499f
amirhosseinghafourian <amirhossein.ghafoorian@gmail.com>
2020-12-19 11:38:18 +0330

    update chat decider

--- DomainModule/src/main/java/com/a/domainmodule/inputValidation/ChatIdDecider.kt
+++ DomainModule/src/main/java/com/a/domainmodule/inputValidation/ChatIdDecider.kt
@@ -2,20 +2,22 @@ package com.a.domainmodule.inputValidation
 
 class ChatIdDecider {
 
-    fun decideChatIdFormat(senderId : String , receiverId : String) : String {
+    fun decideChatIdFormat(senderId: String, receiverId: String): String {
         var senderScore = 0
         var receiverScore = 0
-        repeat(8){
+        repeat(senderId.length) {
             senderScore += scoreSingleChar(senderId[it])
+        }
+        repeat(receiverId.length) {
             receiverScore += scoreSingleChar(receiverId[it])
         }
-        return if (senderScore > receiverScore){
+        return if (senderScore > receiverScore) {
             "$senderId:$receiverId"
-        }else
+        } else
             "$receiverId:$senderId"
     }
 
-    private fun scoreSingleChar(char: Char) : Int{
+    private fun scoreSingleChar(char: Char): Int {
         return char.toInt()
     }
 }
