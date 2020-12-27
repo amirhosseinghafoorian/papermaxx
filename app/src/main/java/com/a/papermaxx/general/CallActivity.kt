2bd1d62dc8c5a04a6e12c65e35c0822148be1e0b
amirhosseinghafourian <amirhossein.ghafoorian@gmail.com>
2020-12-27 11:52:47 +0330

    cleaning video call

--- app/src/main/java/com/a/papermaxx/general/CallActivity.kt
+++ app/src/main/java/com/a/papermaxx/general/CallActivity.kt
@@ -2,14 +2,8 @@ package com.a.papermaxx.general
 
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import androidx.activity.viewModels
-import androidx.navigation.fragment.findNavController
-import com.a.papermaxx.chat.ui.CallFragmentDirections
 import com.a.papermaxx.chat.ui.ChatViewModel
-import com.a.remotemodule.models.CallState
 import org.jitsi.meet.sdk.JitsiMeetActivity
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
 
@@ -23,17 +17,9 @@ class CallActivity : JitsiMeetActivity() {
     override fun onConferenceTerminated(data: MutableMap<String, Any>?) {
         super.onConferenceTerminated(data)
 
-        chatViewModel.endCall(messageSender , chatId)
-        // end call here
+        chatViewModel.endCall(messageSender, chatId)
+
     }
-
-//    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-//        super.onCreate(savedInstanceState, persistentState)
-
-//        chatViewModel.monitorCall(messageReceiver, chatId)
-
-//        monitorCallStatus()
-//    }
 
     fun fillCallValues(chatId: String, messageReceiver: String, messageSender: String) {
         this.chatId = chatId
@@ -46,18 +32,6 @@ class CallActivity : JitsiMeetActivity() {
         intent.action = "org.jitsi.meet.CONFERENCE"
         intent.putExtra("JitsiMeetConferenceOptions", options)
         context.startActivity(intent)
-    }
-
-    private fun monitorCallStatus() {
-        chatViewModel.callStatus.observe(this, { call ->
-
-            if (call == CallState.TALKING) {
-                chatViewModel.establishCall(messageSender, chatId)
-            } else if (call == CallState.END_CALL) {
-                chatViewModel.endCall(messageSender, chatId)
-            }
-
-        })
     }
 
 }
