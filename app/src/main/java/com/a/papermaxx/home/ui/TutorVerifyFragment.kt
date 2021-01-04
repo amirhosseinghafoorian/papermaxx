a2df91b069322f62aafbedf537c719d765a4ae14
amirhosseinghafourian <amirhossein.ghafoorian@gmail.com>
2021-01-04 15:18:13 +0330

    working on verify tutor

---
+++ app/src/main/java/com/a/papermaxx/home/ui/TutorVerifyFragment.kt
@@ -0,0 +1,79 @@
+package com.a.papermaxx.home.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.a.papermaxx.R
+import kotlinx.android.synthetic.main.fragment_tutor_verify.*
+
+class TutorVerifyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_tutor_verify, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        if (false){ // if request has been sent set true
+            btn_verify_tutor.visibility = View.GONE
+            verify_subject_et.visibility = View.GONE
+            verify_education_place_et.visibility = View.GONE
+            verify_message_tv.visibility = View.VISIBLE
+        }
+
+        btn_verify_tutor.setOnClickListener {
+            if (validate()) {
+                btn_verify_tutor.visibility = View.GONE
+                verify_subject_et.visibility = View.GONE
+                verify_education_place_et.visibility = View.GONE
+                verify_message_tv.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun validate(): Boolean {
+        val subject = verify_subject_et.editText?.text.toString()
+        val education = verify_education_place_et.editText?.text.toString()
+        return when {
+            subject.isEmpty() -> {
+                Toast.makeText(requireContext(), "subject can not be empty", Toast.LENGTH_SHORT)
+                    .show()
+                false
+            }
+            subject.length < 3 -> {
+                Toast.makeText(requireContext(), "subject too short", Toast.LENGTH_SHORT)
+                    .show()
+                false
+            }
+            subject.length > 50 -> {
+                Toast.makeText(requireContext(), "subject to long", Toast.LENGTH_SHORT)
+                    .show()
+                false
+            }
+            education.isEmpty() -> {
+                Toast.makeText(requireContext(), "education can not be empty", Toast.LENGTH_SHORT)
+                    .show()
+                false
+            }
+            education.length < 5 -> {
+                Toast.makeText(requireContext(), "subject too short", Toast.LENGTH_SHORT)
+                    .show()
+                false
+            }
+            education.length > 50 -> {
+                Toast.makeText(requireContext(), "subject too long", Toast.LENGTH_SHORT)
+                    .show()
+                false
+            }
+            else -> true
+        }
+    }
+
+}
